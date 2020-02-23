6b347165361073c688658aae5d1e32691f841b59
Fragoso <hfragoso@MXN1800100-2.local>
2020-02-22 22:22:59 -0600

    initial commit

---
+++ xyz-app/xyz-app/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xyz-app
+//
+//  Created by Fragoso, Hector on 2/22/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UIStackView!
+    @IBOutlet weak var zLabel: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
