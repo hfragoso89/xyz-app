d0e57266be6655143e92b495d71fb1b0f01abbcb
Fragoso <hfragoso@MXN1800100-2.local>
2020-02-27 00:04:23 -0600

    added coremotion functionality

--- xyz-app/xyz-app/ViewController.swift
+++ xyz-app/xyz-app/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var xLabel: UILabel!
-    @IBOutlet weak var yLabel: UIStackView!
-    @IBOutlet weak var zLabel: UIStackView!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler:updateLabels)
     }
 
-
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = accelerometerData.acceleration.x
+        let y = accelerometerData.acceleration.y
+        let z = accelerometerData.acceleration.z
+        
+        xLabel.text = "X: \(formatter.string(for: x) ?? "")"
+        yLabel.text = "Y: \(formatter.string(for: y) ?? "")"
+        zLabel.text = "Z: \(formatter.string(for: z) ?? "")"
+    }
 }
 
